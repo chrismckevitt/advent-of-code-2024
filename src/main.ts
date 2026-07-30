81c7359ae03e0add5ccf4651fd28a042a236e51c
Chris <christopher.mckevitt@eonnext.com>
2026-07-30 21:27:36 +0100

    30/7/2026

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,47 @@
-import day1 from "./days/day-1/day-1.ts";
-import day2 from "./days/day-2/day-2.ts";
-import day3 from "./days/day-3/day-3.ts";
-import day4 from "./days/day-4/day-4.ts";
-import day5 from "./days/day-5/day-5.ts";
+import day1 from "./puzzles/day-1/day-1.ts";
+import day2 from "./puzzles/day-2/day-2.ts";
+import day3 from "./puzzles/day-3/day-3.ts";
+import day4 from "./puzzles/day-4/day-4.ts";
+import day5 from "./puzzles/day-5/day-5.ts";
+import { cconsole } from "./utils/cconsole/cconsole.ts";
 
-type DayFn = (input: string) => void;
+type Puzzle = (input: string) => void;
 
-const DAYS = {
+const PUZZLES = {
   day1: [day1, "../input/day-1.json"],
   day2: [day2, "../input/day-2.json"],
   day3: [day3, "../input/day-3.txt"],
   day4: [day4, "../input/day-4.txt"],
   day5: [day5, "../input/day-5.txt"],
-} satisfies Record<string, [DayFn, string]>;
-
-const run = async (
-  fn: DayFn,
-  path: string,
-): Promise<void> => {
-  const input = await Deno.readTextFile(
-    new URL(path, import.meta.url),
-  );
-
-  fn(input);
-};
+} satisfies Record<string, [Puzzle, string]>;
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   console.log("Advent of Code 2024 🎄\n");
 
-  Object.values(DAYS).forEach((day) => {
-    const [fn, path] = day;
+  for (const [puzzle, filepath] of Object.values(PUZZLES)) {
+    await run(puzzle, filepath);
+  }
+}
 
-    void run(fn, path);
-  });
+async function run(
+  puzzle: Puzzle,
+  filepath: string
+) {
+  const input = await getInput(filepath);
+  if (!input) return;
+  puzzle(input);
+
+};
+
+async function getInput(filepath: string) {
+  try {
+    const input = await Deno.readTextFile(
+      new URL(filepath, import.meta.url),
+    );
+
+    return input;
+  } catch (error) {
+    cconsole.error(`Error reading file "${filepath}"\n${JSON.stringify(error, null, 2)}\n`);
+  }
 }
